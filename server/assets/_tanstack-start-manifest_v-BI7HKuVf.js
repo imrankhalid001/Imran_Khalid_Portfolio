fd5eafc652703c9d0d8ce57796d24865c2ce1432
imran <imrankhalid1138903@gmail.com>
2026-06-05 01:00:31 +0500

    Updates

--- server/assets/_tanstack-start-manifest_v-BI7HKuVf.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/testuser/Downloads/Portfolio_website/Imran_Khalid_Portfolio/src/routes/__root.tsx", children: ["/", "/sitemap.xml"], preloads: ["/assets/index-Dypu8CTB.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-Dypu8CTB.js" } }] }, "/": { filePath: "/Users/testuser/Downloads/Portfolio_website/Imran_Khalid_Portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CNA7ypTJ.js"] } } });
-export {
-  tsrStartManifest
-};
